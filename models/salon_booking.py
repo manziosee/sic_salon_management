a8dfe0b7c947c1f111eafcda2faa23bcb2fcc1cb
manzi osee <oseemanzi3@gmail.com>
2025-07-04 21:24:13 +0200

    bug: solving the error on approving the booking

--- models/salon_booking.py
+++ models/salon_booking.py
@@ -1,16 +1,30 @@
-from odoo import models, fields, api
+from odoo import models, fields
+from odoo.exceptions import UserError
 
 class SalonBooking(models.Model):
     _inherit = 'salon.booking'
 
-    stage_id = fields.Many2one(
-        'salon.stage',
-        string='Stage',
-        tracking=True,
-        group_expand='_read_group_stage_ids',
-        default=lambda self: self.env.ref('sic_salon_management.sic_salon_stage_new_request', raise_if_not_found=False),
-    )
+    stage_id = fields.Many2one('salon.stage', string='Stage', index=True, tracking=True)
 
-    @api.model
-    def _read_group_stage_ids(self, stages, domain, order):
-        return self.env['salon.stage'].search([], order='sequence asc')
+    def action_approve(self):
+        Stage = self.env['salon.stage']
+        Order = self.env['salon.order']
+        stage_confirmed = Stage.search([('name', '=', 'Confirmed')], limit=1)
+        if not stage_confirmed:
+            raise UserError('Cannot approve salon booking because the "Confirmed" stage is missing.')
+
+        for booking in self.filtered(lambda b: b.state != 'approved'):
+            order_vals = {
+                'name': self.env['ir.sequence'].next_by_code('salon.order') or 'New',
+                'customer_name': booking.name,
+                'time_start': booking.datetime,
+                'salon_chair_id': booking.salon_chair_id.id,
+                'phone': booking.phone,
+                'email': booking.email,
+                'stage_id': stage_confirmed.id,
+            }
+            Order.create(order_vals)
+            booking.stage_id = stage_confirmed
+            booking.state = 'approved'
+
+        return True
